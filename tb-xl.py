98ee4234c298ae8ae488fd33c1e7acdc585acf22
haha1379 <hugsun3.141@gmail.com>
2020-04-25 23:53:41 +0800

    Update tb-xl.py

--- tb-xl.py
+++ tb-xl.py
@@ -4,7 +4,7 @@ import re
 
 def gethtmltext(url):
     kv = {
-        'cookie': 'cna=CuT8FkWvqSoCAW64tpRihema; sca=97903e99; cnaui=1672437537; aui=1672437537; cdpid=UoezTUqAaxQtoQ%253D%253D; cmida=1401053355_20200331114015; cap=5c45; cad=171a5f8ba82-5923847864888969240001; tbsa=98dedf46f7f5a9c86a52a64a_1587627620_4; atpsida=c825589165425aaf48eaf276_1587627620_4; atpsidas=54f1d01088ce7582ad801344_1587627620_5',
+        'cookie': '',
         'referer': 'https://s.taobao.com/search?q=ipad&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20200423&ie=utf8',
         'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'
         }
